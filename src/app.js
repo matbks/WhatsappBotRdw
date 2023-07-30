0ac651fcc8e8b5e94427b21cb081d222555fdecf
Backhaus <44726485+matbks@users.noreply.github.com>
2023-07-29 23:58:47 -0300

    v

--- src/app.js
+++ src/app.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  { urlencoded, json } = require("body-parser"),
+  app = express().use(bodyParser.json());
+      
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// Verify that the callback came from Facebook.
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature-256"];
 
-app.post('/webhook', (req, res) => {
-  const messages = req.body.messages;
-  
-  messages.forEach(message => {
-    if (!message.fromMe) {
-      console.log('Received message from ' + message.author);
-      console.log('Message content: ' + message.body.text.body);
-      // Here you can add your logic to handle incoming messages
+  if (!signature) {
+    console.warn(`Couldn't find "x-hub-signature-256" in headers.`);
+  } else {
+    var elements = signature.split("=");
+    var signatureHash = elements[1];
+    var expectedHash = crypto
+      .createHmac("sha256", config.appSecret)
+      .update(buf)
+      .digest("hex");
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
     }
-  });
-
-  res.status(200).send('Messages received!');
-});
-
-app.listen(3002, () => {
-  console.log('WhatsApp Bot is listening to port 3002');
-});
+  }
+}
 
 
 // const express = require('express');
