dcf94f9b2cfddf33b26512721ddb898e2f890129
Backhaus <44726485+matbks@users.noreply.github.com>
2023-08-02 11:38:52 -0300

    ss

--- src/app2.js
+++ src/app2.js
@@ -1,71 +1,40 @@
 const express = require('express');
-const samlify = require('samlify');
-const axios = require('axios');
-const querystring = require('querystring');
-
+const passport = require('passport');
+const OAuth2Strategy = require('passport-oauth2');
+const session = require('express-session');
 const app = express();
 
-// SP (Service Provider) configuration
-const sp = samlify.ServiceProvider({
-  entityID: 'saml-poc',
-  authnRequestsSigned: false,
-  wantAssertionsSigned: true,
-  privateKey: '<Your SP Private Key>', // Replace with your actual SP private key
-  privateKeyPass: 'password', // Replace with your actual password
-  assertionConsumerService: [
-    {
-      Binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-      Location: 'http://localhost:7000/acs'
-    }
-  ],
-  singleLogoutService: [
-    {
-      Binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-      Location: 'http://localhost:7000/slo'
-    }
-  ],
+app.use(session({ secret: 'your secret here', resave: false, saveUninitialized: false }));
+
+const oauth2Strategy = new OAuth2Strategy({
+  authorizationURL: 'https://vsapdev.tipler.com.br:8043/sap/bc/sec/oauth2/authorize?sap-client=100',
+  tokenURL: 'https://vsapdev.tipler.com.br:8043/sap/bc/sec/oauth2/token?sap-client=100',
+  clientID: 'ODATA_RED',
+  scope: 'ZTESTE_SRV_0001',
+  callbackURL: 'https://bot.redware.io/auth/callback',
+  state: true
+},
+function(accessToken, refreshToken, profile, cb) {
+  console.log('Access Token:', accessToken);
+  console.log('Refresh Token:', refreshToken);
+  console.log('Profile:', profile);
+  return cb(null, profile);
 });
 
-// IdP (Identity Provider) configuration
-const idp = samlify.IdentityProvider({
-  metadata: '<Your IdP Metadata XML>', // Replace with your actual IdP metadata XML
-  wantAuthnRequestsSigned: true,
-  wantLogoutResponseSigned: true,
-  wantLogoutRequestSigned: true,
-});
+passport.use(oauth2Strategy);
 
-// Set up Express routes
-app.get('/login', async (req, res) => {
-  try {
-    const { context } = await sp.createLoginRequest(idp, 'redirect');
-    return res.redirect(context);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send('Internal Server Error');
+app.get('/auth/callback',
+  passport.authenticate('oauth2', { session: false }),
+  function(req, res) {
+    res.send('OAuth2 authentication successful!');
   }
-});
+);
 
-app.post('/acs', async (req, res) => {
-  try {
-    const { extract } = await sp.parseLoginResponse(idp, 'post', req);
-    // Perform additional actions after successful login
-    return res.send('Login successful!');
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send('Internal Server Error');
-  }
-});
+app.get('/auth',
+  passport.authenticate('oauth2', { session: false })
+);
 
-app.post('/slo', async (req, res) => {
-  try {
-    await sp.parseLogoutRequest(idp, 'post', req);
-    // Perform additional actions after successful logout
-    return res.send('Logout successful!');
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send('Internal Server Error');
-  }
+app.use(passport.initialize());
+app.listen(3002, () => {
+  console.log('Server started on port 3002');
 });
-
-// Start the server
-app.listen(7000, () => console.log('Server started on port 7000'));
