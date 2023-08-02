4076188750abfc19542b3fd5ed97eacd8b57cb8f
Backhaus <44726485+matbks@users.noreply.github.com>
2023-08-02 11:50:12 -0300

    ss

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/auth', (req, res) => {
-  const authURL = `https://vsapdev.tipler.com.br:8043/sap/bc/sec/oauth2/authorize?sap-client=100&response_type=token&client_id=ODATA_RED&redirect_uri=https://bot.redware.io/auth/callback&scope=ZTESTE_SRV_0001&state=YOUR_STATE_VALUE`;
+  const authURL = `https://vsapdev.tipler.com.br:8043/sap/bc/sec/oauth2/authorize?sap-client=100&response_type=token&client_id=ODATA_RED&redirect_uri=https://bot.redware.io/auth/callback&scope=ZTESTE_SRV_0001&state=1599`;
   res.redirect(authURL);
 });
 
@@ -13,7 +13,7 @@ app.get('/auth/callback', (req, res) => {
   // If the state values match, the request was made by your app
   // If not, the request could be a CSRF attack, do not process it
   // if (state === 'YOUR_STATE_VALUE') {
-  if (state) {
+  if (state = 1599) {
     // The access token is available in the access_token query parameter
     // You can now use this access token to make authenticated requests to the resource server
     res.send(`OAuth2 authentication successful! Access token: ${accessToken}`);
